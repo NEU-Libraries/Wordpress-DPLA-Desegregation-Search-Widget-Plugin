71b41e75c8f88f62e9dd0681ccf247b1ff11170f
Larry Farrell <farrelldlfarrell@gmail.com>
2014-01-02 21:48:09 -0500

    Should do it for the widget

--- wp_dpla_search_widget.php
+++ wp_dpla_search_widget.php
@@ -7,3 +7,10 @@ Author: Dean Farrell
 Version: 0.1
 Author URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
 */
+
+function dpla_search_widget_load() {
+    wp_register_script('add-dpla-widget-js', 'http://dp.la/info/wp-content/uploads/2013/12/dpla-search-widget.js', '', null,'');
+    wp_enqueue_script('add-dpla-widget-js');
+}
+
+add_action( 'wp_enqueue_scripts', 'dpla_search_widget_load');
