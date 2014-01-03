11b7f9ace1eef9cd514ac3b1fa4a28a6d5c8f71f
Larry Farrell <farrelldlfarrell@gmail.com>
2014-01-02 21:19:10 -0500

    Basic plugin layout

---
+++ wp_dpla_search_widget.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: DPLA Search Widget
+Plugin URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
+Description: Plugin for displaying a search form to search the Digital Public Library of America
+Author: Dean Farrell
+Version: 0.1
+Author URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
+*/
