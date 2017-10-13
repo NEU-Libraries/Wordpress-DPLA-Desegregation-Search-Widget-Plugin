930b530854eff7fa088f71480df2283e722d3a39
nestochan <e.valencia@neu.edu>
2017-10-13 12:45:48 -0400

    Update wp_dpla_search_widget.php
    updated js URI

--- wp_dpla_search_widget.php
+++ wp_dpla_search_widget.php
@@ -1,15 +1,16 @@
 <?php
 /*
-Plugin Name: DPLA Search Widget
-Plugin URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
-Description: Plugin for displaying a search form to search the Digital Public Library of America
+Original Plugin Name: DPLA Search Widget
+NEW Plugin Name: DPLA Desegration Search Widget
+Original Plugin URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
+Description: Plugin for displaying a search form to search the Digital Public Library of America and concatenating relevant Boston Desegreation materials search string
 Author: Dean Farrell
 Version: 0.1
-Author URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
+Original Author URI: https://github.com/lfarrell/Wordpress-DPLA-Search-Widget-Plugin
 */
 
 function dpla_search_widget_load() {
-    wp_register_script('add-dpla-widget-js', 'http://dp.la/info/wp-content/uploads/2013/12/dpla-search-widget.js', '', null,'');
+    wp_register_script('add-dpla-widget-js', 'http://liblab.neu.edu/dpla-widget/desegregation/dpla-deseg-search-widget.js', '', null,'');
     wp_enqueue_script('add-dpla-widget-js');
 }
 
